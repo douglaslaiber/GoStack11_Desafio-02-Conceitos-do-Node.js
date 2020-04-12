ac6ac6891d44bb9db373bd6018dd21a7e381b6b3
Douglas Laiber <douglaslbosco@gmail.com>
2020-04-12 13:12:44 -0300

    Feat - Implementation of route features

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,64 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const repositoryId = request.params.id;
+  const { title, url, techs } = request.body;
+
+  if (!isUuid(repositoryId)) {
+    return response.status(400).json({ "error": "Repository id is not valid" });
+  }
+
+  let repositoryFound = repositories.find(repo => repo.id === repositoryId);
+  repositoryFound.title = title;
+  repositoryFound.url = url;
+  repositoryFound.techs = techs;
+
+  return response.json(repositoryFound);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const repositoryId = request.params.id;
+
+  if (!isUuid(repositoryId)) {
+    return response.status(400).json({ "error": "Repository id is not valid" });
+  }
+
+  let repositoryIndex = repositories.findIndex(repo => repo.id === repositoryId);
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const repositoryId = request.params.id;
+
+  if (!isUuid(repositoryId)) {
+    return response.status(400).json({ "error": "Repository id is not valid" });
+  }
+
+  let repositoryFound = repositories.find(repo => repo.id === repositoryId);
+  repositoryFound.likes++;
+
+  return response.json(repositoryFound);
 });
 
 module.exports = app;
